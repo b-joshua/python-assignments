3770ac135a518afa1b40569481d8a7f7fd851977
b-joshua <joshua.bugis@weizmann.ac.il>
2024-11-12 14:33:22 +0200

    Add files via upload
    Day 2 assignments

---
+++ circle.py
@@ -0,0 +1,6 @@
+radius = 10
+pi = 3.14
+area = pi * (radius**2)
+print(area)
+circumference = pi * 2 * radius
+print(circumference)
---
+++ rectangle.py
@@ -0,0 +1,6 @@
+width = 5
+length = 10
+area = width * length
+print(area)
+circumference = 2*width + 2*length
+print(circumference)
